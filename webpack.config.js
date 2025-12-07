d7cdcc436214f9f3a3f40d9c79368915bf321ccc
Riddhi <riddhipatel2525@gmail.com>
2025-12-06 21:00:02 -0500

    rebuild

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -38,6 +39,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
+  ],
+
+   plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "public/_redirects", to: "" }
+      ]
+    })
   ],
 
   devServer: {
